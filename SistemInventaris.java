fd2dc187b2fdc7129f874462ee0672e73a909806
AdriFavian <adrifavian2@gmail.com>
2023-10-31 23:39:11 +0700

    update: input barang masuk dan login

--- SistemInventaris.java
+++ SistemInventaris.java
@@ -4,25 +4,33 @@ public class SistemInventaris {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //Array user dan gudang penyimpanan
         String[][] userArr = new String[10][3]; // Array dua dimensi: username, password, role
-        String[][] gudangArr = new String[5][4];
-        int userCount = 0;
+        String[][] gudangArr = new String[100][3]; // Array dua dimensi: nama, jumlah, tanggal
         boolean IsLogin = false;
         boolean IsAdmin = false;
         boolean IsStaff = false;
+        int userCount = 0;
+        int gudangCount = 0;
         String currentUser = "";
 
+        //Warna Teks
+        String RESET = "\u001B[0m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        
         while (!IsLogin) {
-            System.out.println("===================================");
-            System.out.println("SELAMAT DATANG DI SISTEM INVENTARIS");
-            System.out.println("===================================");
+            System.out.println("===================================\n");
+            System.out.println(YELLOW+"SELAMAT DATANG DI SISTEM INVENTARIS"+RESET);
+            System.out.println("\n===================================");
             System.out.println("1. Buat akun");
             System.out.println("2. Login");
             System.out.println("3. Tampilkan pengguna");
             System.out.println("4. Keluar");
-            System.out.print("Pilih Menu: ");
+            System.out.print("\nPilih Menu: ");
             int choice = sc.nextInt();
-
+            
             if (choice == 1) {
                 // Menu Buat Akun
                 System.out.println("===================================");
@@ -34,15 +42,20 @@ public class SistemInventaris {
                 String password = sc.next();
                 System.out.print("Role (staff/admin): ");
                 String role = sc.next();
+                
+                for (int l=0; l< userArr.length; l++) {
+                    if (userArr[l][0] == null) {
+                        userArr[l][0] = username;
+                        userArr[l][1] = password;
+                        userArr[l][2] = role;
+                        userCount++;
+                        break; 
+                    }
+                }
 
-                userArr[userCount][0] = username;
-                userArr[userCount][1] = password;
-                userArr[userCount][2] = role;
-                userCount++;
-
-                System.out.println("\n");
-                System.out.println("       AKUN BERHASIL DIBUAT !      ");
-                System.out.println("\n");
+                System.out.println("===================================");
+                System.out.println(GREEN+"       AKUN BERHASIL DIBUAT !      "+RESET);
+                System.out.println("===================================");
 
             } else if (choice == 2) {
                 // Menu Login
@@ -54,7 +67,7 @@ public class SistemInventaris {
                 System.out.print("Password: ");
                 String password = sc.next();
                 
-                for (int i = 0; i < userCount; i++) {
+                for (int i = 0; i < userArr.length; i++) {
                     if (userArr[i][0].equals(username) && userArr[i][1].equals(password)) {
                         IsLogin = true;
                         currentUser = username;
@@ -77,7 +90,7 @@ public class SistemInventaris {
 
                     System.out.println("1. Input Barang Masuk");
                     System.out.println("2. Input Barang Keluar");
-                    System.out.println("Pilih Menu: ");
+                    System.out.print("\nPilih Menu: ");
                     int choicee = sc.nextInt();
 
                     switch (choicee) {
@@ -86,15 +99,28 @@ public class SistemInventaris {
                             System.out.println("         Input Barang Masuk        ");
                             System.out.println("===================================");
 
-                            System.out.println("Nama barang: ");
-                            String namaBarang = sc.next();
-                            System.out.println("Jumlah barang: ");
-                            int jumlahBarang = sc.nextInt();
-                            System.out.println("Tanggal barang masuk: ");
-                            String tanggalMasuk = sc.next();
+                            System.out.print("Berapa jenis barang yang ingin diinput: ");
+                            int jumlahInput = sc.nextInt();
 
-                            
-
+                            for (int k=0; k<jumlahInput; k++) {
+                                System.out.println("Data barang ke - "+(k+1));
+                                System.out.print("Nama barang: ");
+                                String namaBarang = sc.next();
+                                System.out.print("Jumlah barang: ");
+                                int jumlahBarang = sc.nextInt();
+                                System.out.print("Tanggal barang masuk: ");
+                                String tanggalMasuk = sc.next();
+                                
+                                String informasiBarangMasuk = "Nama: " + namaBarang + " Jumlah: " + jumlahBarang + " Tanggal Masuk: " + tanggalMasuk;
+                                for (int i = 0; i < gudangArr.length; i++) {
+                                    if (gudangArr[i][0] == null) {
+                                        gudangArr[i][0] = informasiBarangMasuk;
+                                        gudangCount++;
+                                        break; 
+                                    }
+                                }
+                                System.out.println("\nData barang masuk telah disimpan.\n");
+                            }
                         break;
                     } 
                     break;
@@ -115,9 +141,11 @@ public class SistemInventaris {
                 System.out.println("            DAFTAR AKUN            ");
                 System.out.println("===================================");
                 for (int i = 0; i < userCount; i++) {
-                    System.out.println("Username: " + userArr[i][0] + " Role: " + userArr[i][2]);
+                    System.out.println("Username: " + userArr[i][0] + 
+                                        " Password: " + userArr[i][1] +
+                                        " Role: " + userArr[i][2] + "\n");
                 }
-                System.out.println("\n");
+
             } else if (choice == 4) {
                 break;
             } else {
